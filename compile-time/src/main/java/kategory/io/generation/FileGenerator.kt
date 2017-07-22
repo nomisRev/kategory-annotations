3ae0b4d0bccf3758a5c54d8e01ef25dc294f90ad
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-07-22 11:41:00 +0200

    fix KotlinPoet generator

--- compile-time/src/main/java/kategory/io/generation/FileGenerator.kt
+++ compile-time/src/main/java/kategory/io/generation/FileGenerator.kt
@@ -8,23 +8,14 @@ import com.squareup.kotlinpoet.*
 class FileGenerator {
 
     fun createKotlinFile(): KotlinFile {
-        val greeterClass = ClassName("", "Greeter")
-
-        return KotlinFile.builder("", "HelloWorld")
-                .addType(TypeSpec.classBuilder("Greeter")
+        return KotlinFile.builder("kategory.io", "HelloWorld")
+                .addType(TypeSpec.classBuilder("HelloWorld")
                         .primaryConstructor(FunSpec.constructorBuilder()
                                 .addParameter("name", String::class)
                                 .build())
                         .addProperty(PropertySpec.builder("name", String::class)
                                 .initializer("name")
                                 .build())
-                        .addFun(FunSpec.builder("greet")
-                                .addStatement("println(%S)", "Hello, \$name")
-                                .build())
-                        .build())
-                .addFun(FunSpec.builder("main")
-                        .addParameter("args", String::class, KModifier.VARARG)
-                        .addStatement("%T(args[0]).greet()", greeterClass)
                         .build())
                 .build()
     }
