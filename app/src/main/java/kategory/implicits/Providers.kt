2eca9eed9712ac32bcb025b93ca178743cefdb2c
Raul Raja <raulraja@gmail.com>
2017-08-07 14:44:10 +0200

    Removed recursive implicits test

--- app/src/main/java/kategory/implicits/Providers.kt
+++ app/src/main/java/kategory/implicits/Providers.kt
@@ -4,8 +4,6 @@ import kategory.implicit
 
 @implicit fun provideString() = "1"
 
-@implicit fun recursiveImplicit(@implicit S: String): Int = S.toInt()
-
 @implicit fun provideIntList(): List<Int> = listOf(1,2,3)
 
 object Wrapper {
