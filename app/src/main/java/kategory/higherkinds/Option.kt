a63574bbfb25f0b8f380669f9a15f30ccd7c60f6
Raul Raja <raulraja@gmail.com>
2017-08-12 22:26:31 +0200

    Generates partially applied type aliases for kinds w/ arity > 1

--- app/src/main/java/kategory/higherkinds/Option.kt
+++ app/src/main/java/kategory/higherkinds/Option.kt
@@ -5,3 +5,10 @@ import kategory.higherkind
 @higherkind sealed class Option<out A> : OptionKind<A>
 
 @higherkind sealed class Either<L, R> : EitherKind<L, R>
+
+@higherkind sealed class StateT<F, S, A> : StateTKind<F, S, A>
+
+typealias X<L> = EitherKindPartiallyApplied<L>
+
+typealias Z<F, S> = StateTKindPartiallyApplied<F, S>
+
