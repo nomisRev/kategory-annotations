2c828103862977d1618bcc566c9acc3a7bc94731
pakoito <jueces.jus@gmail.com>
2017-08-20 19:20:53 +0100

    Add alias

--- annotations/src/main/java/kategory/hks.kt
+++ annotations/src/main/java/kategory/hks.kt
@@ -1,5 +1,7 @@
 package kategory
 
+import io.kindedj.HK as HK_J
+
 interface HK<out F, out A>
 
 typealias HK2<F, A, B> = HK<HK<F, A>, B>
@@ -9,3 +11,11 @@ typealias HK3<F, A, B, C> = HK<HK2<F, A, B>, C>
 typealias HK4<F, A, B, C, D> = HK<HK3<F, A, B, C>, D>
 
 typealias HK5<F, A, B, C, D, E> = HK<HK4<F, A, B, C, D>, E>
+
+typealias HK2_J<F, A, B> = HK_J<HK_J<F, A>, B>
+
+typealias HK3_J<F, A, B, C> = HK_J<HK2<F, A, B>, C>
+
+typealias HK4_J<F, A, B, C, D> = HK_J<HK3<F, A, B, C>, D>
+
+typealias HK5_J<F, A, B, C, D, E> = HK_J<HK4<F, A, B, C, D>, E>
